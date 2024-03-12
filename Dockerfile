6f876cd94033584d8118902bde9f83bb362d9583
zjj-desktop <zhaojunjie0206@live.cn>
2024-03-12 22:53:01 +0800

    update

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM ubuntu:latest
+LABEL authors="Junjie"
+
+ENTRYPOINT ["top", "-b"]
--- main.go
+++ main.go
@@ -1,6 +1,21 @@
 package main
 
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "web/home.html")
+}
+
 // start service:
 func main() {
+	port := flag.String("port", "5678", "http service port")
+	flag.Parse()
+
+	http.HandleFunc("/", serveHome)
+	log.Printf("http serve on port %s\n", *port)
 
 }
